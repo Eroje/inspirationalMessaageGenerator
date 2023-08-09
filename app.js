ae5bb7b8d5e3d79a07e82c222816a3cc9da1b9a5
Mojid Eroje <majidy287@gmail.com>
2023-08-09 07:32:13 +0100

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const faker = require('faker');
 
-// Lists of data
 const adjectives = [
   "Amazing", "Brilliant", "Courageous", "Determined", "Empowering",
   "Fierce", "Grateful", "Hopeful", "Inspiring", "Joyful"
@@ -22,15 +21,14 @@ const phrases = [
   "Your potential is limitless, keep pushing forward."
 ];
 
-// Function to generate a random message
 function generateRandomMessage() {
-  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  const randomAdjective = faker.random.arrayElement(adjectives);
+  const randomNoun = faker.random.arrayElement(nouns);
+  const randomPhrase = faker.random.arrayElement(phrases);
 
   const message = `${randomAdjective} ${randomNoun}: ${randomPhrase}`;
   return message;
 }
 
-// Generate and display a random message
-console.log(generateRandomMessage());
+const randomMessage = generateRandomMessage();
+console.log(randomMessage);
